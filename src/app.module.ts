c887d9b11438f11a35d928d0e1f07aeb50056f42
Damian <skolnikdamian2@gmail.com>
2025-09-12 11:05:27 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { PostModule } from './post/post.module';
+import { UserModule } from './modules/user/user.module';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
   imports: [UserModule, PostModule],
